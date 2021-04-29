89a899da41a5fe3f861db9da5066fbfed74bf35f
Pauline Bacle <bacle-pauline@orange.fr>
2021-04-29 11:41:44 +0200

    test ok // Need to add verification for inputs

--- backend/app.js
+++ backend/app.js
@@ -23,12 +23,11 @@ app.use((req, res, next) => {
   next();
 });
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/sauce', saucesRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
